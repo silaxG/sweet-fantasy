3098cdd81ff640568f09838767ef8e20d5d68860
silaxG <gastonizurita@gmail.com>
2024-11-06 09:51:11 -0300

    cambiando el codigo de tabs a tab1 por una confusion tonta XD

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Receta } from '../models/item';
+import { FirestoreService } from '../services/firestore.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private database: FirestoreService) {
+  }
+  receta: Receta[]= []
+
+  ngOnInit(){
+    this.obtenerresultado()
+    
+  }
+
+  obtenerresultado(){
+    this.database.getColection<Receta>('receta').subscribe(res =>{console.log("informacion devuelta ",res)
+
+      this.receta= res;
+    })
+  }
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Receta } from '../models/item';
-import { FirestoreService } from '../services/firestore.service';
- 
+
 
 @Component({
   selector: 'app-tabs',
@@ -10,19 +8,5 @@ import { FirestoreService } from '../services/firestore.service';
 })
 export class TabsPage {
 
-  constructor(private database: FirestoreService) {
-  }
-  receta: Receta[]= []
-
-  ngOnInit(){
-    this.obtenerresultado()
-    
-  }
-
-  obtenerresultado(){
-    this.database.getColection<Receta>('receta').subscribe(res =>{console.log("informacion devuelta ",res)
-
-      this.receta= res;
-    })
-  }
+ 
 }
